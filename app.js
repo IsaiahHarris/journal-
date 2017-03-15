de53263ebf848030c05832a9273249b8b4c2b1c0
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-03-15 09:11:51 -1000

    dynamic

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var jsonBody = [
+// var jsonBody = [
+//   {
+//     "header":"center"
+//   }
+//   {
+//     "div"{
+//       "class":"container"
+//     }
+//   }
+//   {
+//     "div": {
+//       "class":"article",
+//       [
+//       "h3": "why i took this class",
+//       {"p" : "i took this class because it seemed cool and my friends were taking it and i didnt know what else elective to take.im an "}]
+//     }
+//   }
+// ];
+
+var simplifiedJson =[
   {
-    "header":"center"
+    "journalEntry": "1",
+    "date": "aug 15",
+    "title": "my first entry",
+    "content": "i took this class because it seemed cool and my friends were taking it and i didnt know what else elective to take"
   }
   {
-    "div"{
-      "class":"container"
-    }
+    "journalEntry": "2",
+    "date": "10/7/16",
+    "title":"Digimon Go"
+    "content":"Digimon Go project or assignment really opened my eyes to how to do everything we learned so far. After school when i worked on it for some reason a lightbulb went off in my head and i got the connection from the html and css. Even thoiugh it was really easy it just gave me a better understanding of everything that we learned"
   }
   {
-    "div": {
-      "class":"article",
-      [
-      "h3": "why i took this class",
-      {"p" : "i took this class because it seemed cool and my friends were taking it and i didnt know what else elective to take.im an "}]
-    }
+    
   }
-];
+  ]
