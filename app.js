60e8b82184dc2b9a405f5e6fcfc8f8b1a49ecd4d
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-03-15 08:45:50 -1000

    dynamicness

---
+++ app.js
@@ -0,0 +1,18 @@
+var jsonBody = [
+  {
+    "header":"center"
+  }
+  {
+    "div"{
+      "class":"container"
+    }
+  }
+  {
+    "div": {
+      "class":"article",
+      [
+      "h3": "why i took this class",
+      {"p" : "i took this class because it seemed cool and my friends were taking it and i didnt know what else elective to take.im an "}]
+    }
+  }
+];
